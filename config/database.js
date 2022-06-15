db307b15b8a44eb0c54ccc3c1119d00f81434e00
andypoquis <andrespoquis@gmail.com>
2022-06-15 03:14:28 +0000

    a

--- config/database.js
+++ config/database.js
@@ -2,9 +2,9 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '34.133.19.162'),
+      host: env('DATABASE_HOST', '34.71.41.154'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'mecanicawinderdb'),
+      database: env('DATABASE_NAME', 'postgres'),
       user: env('DATABASE_USERNAME', 'postgres'),
       password: env('DATABASE_PASSWORD', '123456'),
       ssl: env.bool('DATABASE_SSL', false),
